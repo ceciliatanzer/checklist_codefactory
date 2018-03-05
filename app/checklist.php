8fb14057f086f0de1ce02eef3c2313f56fbfb6f9
[gregormosthammer] <[g.mosthammer@gmail.com]>
2018-03-05 14:02:08 +0100

    update model checklist

--- app/checklist.php
+++ app/checklist.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class checklist extends Model
 {
     protected $table = 'checklist';
-    protected $fillable = array('passport', 'financing', 'insurance', 'pass', 'visaform', 'picture', 'travelreservation', 'hotelreservation', 'employment', 'travelinsurance', 'financial_means', 'passcopy', 'consularfee', 'user_id');
+    protected $fillable = array('passport', 'financing', 'insurance', 'pass', 'visaform', 'picture', 'travelreservation', 'hotelreservation', 'employment', 'travelinsurance', 'financial_means', 'passcopy', 'consularfee', 'user_id', 'students_id');
 
     /**
      * Create or update a record matching the attributes, and fill it with values.
