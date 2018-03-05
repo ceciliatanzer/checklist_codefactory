8da9f1c81fc87284b46a6bb726cb15f10dd61525
Your Name <lil_tanboora@icloud.com>
2018-03-05 16:20:39 +0100

    welcome page responsive with comments

--- public/js/app.js
+++ public/js/app.js
@@ -32041,7 +32041,7 @@ var Component = __webpack_require__(37)(
   /* cssModules */
   null
 )
-Component.options.__file = "C:\\Users\\Gregor\\Desktop\\checklist_codefactory_backup\\resources\\assets\\js\\components\\Example.vue"
+Component.options.__file = "C:\\xampp\\htdocs\\checklist\\resources\\assets\\js\\components\\Example.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Example.vue: functional components are not supported with templates, they should use render functions.")}
 
@@ -32052,9 +32052,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-f725f0ae", Component.options)
+    hotAPI.createRecord("data-v-15c7050c", Component.options)
   } else {
-    hotAPI.reload("data-v-f725f0ae", Component.options)
+    hotAPI.reload("data-v-15c7050c", Component.options)
   }
 })()}
 
@@ -32139,7 +32139,7 @@ module.exports.render._withStripped = true
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-     require("vue-hot-reload-api").rerender("data-v-f725f0ae", module.exports)
+     require("vue-hot-reload-api").rerender("data-v-15c7050c", module.exports)
   }
 }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,104 +19,115 @@
 
 
 
-
+<!-- container opened -->
 <div class="container-fluid">
-<div class="row w3-container w3-center" style="padding:70px 16px">
-<div class="col-lg-6 col-lg-offset-3">
+    <!-- for the red area -->
+    <div class="row w3-container w3-center" style="padding:70px 16px">
+    <div class="col-lg-6 col-lg-offset-3">
 
-  <h1 class="w3-jumbo">What are you waiting for?</h1>
-  <div class="w3-xlarge">
-  
-    <p>Average Full-Stack Developer Salary $88,488</p>
-    <p>In-Demand Career</p>
-    <p>Life Changing Experience</p>
+    <h1 class="welchead">What are you waiting for?</h1>
+    <div class="w3-xlarge">
     
-    
-      
+        <p>Average Full-Stack Developer Salary $88,488</p>
+        <p>In-Demand Career</p>
+        <p>Life Changing Experience</p>
+        
     </div>
-    <a href="{{url('/register')}}" class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Sign up</a>
-    <h3>Refer a friend and receive 10% off your cost up to $10,000</h3>
+    <!-- the button that send u to register -->
+        <a href="{{url('/register')}}" class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Sign up</a>
+        <h3>Refer a friend and receive 10% off your cost up to $10,000</h3>
 
-</div>
-</div>
-
+    </div>
+    </div>
+    <!-- end of the the red area -->
 
 <br>
 
 
+
+<!-- new row for chris with the pics -->
+<div class="row">
+    <!-- the whole grey box only for chris -->
+<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 polaroid">
+    <!-- col for chris pic with text inside after hover -->
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 con">
+        <img src="/img\chris.png" alt="CTO" width="100%" height="100%">
+        <div class="overlay">
+            <div class="text">-Christoph Pirringer <br> CTO</div>
+        </div>
+    </div> <!-- end for the pic div -->
+    <!-- chris text -->
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 cont">
+        <h3>"Coding is THE most important skill of the 21st century. With it, you can change everything."</h3>
+    </div>
+</div> <!-- end of the grey box for chris -->
+</div> <!-- end of chris row -->
+
+
+<!-- new row for student with the pics -->
 <div class="row">
 <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 polaroid">
+    <!-- text for the student -->
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 cont">
+        <h3>"At CodeFactory, I learned more professional skills in such a short time than I ever thought possible. Even before the End of the course, I had companies looking to hire me!"</h3>
 
-<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 con">
-
-<img src="/img\chris.png" alt="CTO" width="100%" height="100%">
-<div class="overlay">
-    <div class="text">-Christoph Pirringer <br> CTO</div>
-  </div>
-</div>
-<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 cont">
-<h3>"Coding is THE most important skill of the 21st century. With it, you can change everything."</h3>
-</div>
-</div>
-</div>
+    </div>
+        <!-- col for student pic with text inside after hover -->
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 con">
+        <img src="/img\stu.png" alt="CTO" width="100%" height="100%">
+        <div class="overlay">
+            <div class="text">-Simona Mirauta</div>
+        </div>
+    </div>
+</div><!-- end of the grey box for student -->
+</div><!-- end of student row -->
 
 
+
+
+<!-- row for header text for package and perks --> 
 <div class="row">
-<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1 polaroid">
-<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 cont">
-<h3>"At CodeFactory, I learned more professional skills in such a short time than I ever thought possible. Even before the End of the course, I had companies looking to hire me!"</h3>
-
-</div>
-<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 con">
-
-<img src="/img\stu.png" alt="CTO" width="100%" height="100%">
-<div class="overlay">
-    <div class="text">-Simona Mirauta</div>
-  </div>
-</div>
-</div>
+    <div class="col-lg-offset-3 col-lg-6">
+        <h2 style="color: #831213; font-family: Chivo !important; text-align: center">Get a new career and the experience of a lifetime </h2>
+    </div>
 </div>
 
 
 
-</div>
-
+<!-- new row for the package and perks text -->
 <div class="row">
+    <!-- big col with offset -->
+    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1">
+        
+    <!-- col for packages -->
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <h3 style="color: #831213; font-family: Chivo !important">The Package:</h3>
+            <ul>
+                <li>4 Months Full-Time Course in Vienna</li>
+                <li>Housing and Flights</li>
+                <li>HTML5, CSS3, JavaScript, JQuery, Bootstrap, Project Documentaion, TypeScript, AngularJS 4.0</li>
+                <li>SQL(MySQL), SCRUM, PHP, AJAX, Symphony PHP framework, WorldPress, Software Testing AND Many other Skills</li>
+                <li>Only $18.500</li>
+            </ul>
+        </div>
 
-<div class="col-lg-offset-3 col-lg-6">
-<h2 style="color: #831213; font-family: Chivo !important; text-align: center">Get a new career and the experience of a lifetime </h2>
-</div>
-</div>
+        <!-- col for perks -->
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <h3 style="color: #831213; font-family: Chivo !important">Additional Perks:</h3>
+            <ul>
+                <li>Graduation in Salzburg, Austria</li>
+                <li>Food and Insurance</li>
+                <li>Finals Week on the beach</li>
+                <li>Tours, Hackthons, enjoying Vienna's nightlife, and other fun group activities</li>
+            </ul>
+        </div>
 
-<div class="row">
-<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-</div>
+    </div> <!-- end for the big col -->
 
-<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-    <h3 style="color: #831213; font-family: Chivo !important">The Package:</h3>
-    <ul>
-        <li>4 Months Full-Time Course in Vienna</li>
-        <li>Housing and Flights</li>
-        <li>HTML5, CSS3, JavaScript, JQuery, Bootstrap, Project Documentaion, TypeScript, AngularJS 4.0</li>
-        <li>SQL(MySQL), SCRUM, PHP, AJAX, Symphony PHP framework, WorldPress, Software Testing AND Many other Skills</li>
-        <li>Only $18.500</li>
-    </ul>
-</div>
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+    </div>
 
-<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
-    <h3 style="color: #831213; font-family: Chivo !important">Additional Perks:</h3>
-    <ul>
-        <li>Graduation in Salzburg, Austria</li>
-        <li>Food and Insurance</li>
-        <li>Finals Week on the beach</li>
-        <li>Tours, Hackthons, enjoying Vienna's nightlife, and other fun group activities</li>
-    </ul>
-</div>
-
-<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-</div>
-
-</div>
+</div><!-- end package and perks -->
 
 
 
@@ -125,19 +136,20 @@
 
 
 <div class="row">
+    <!-- big img at the end -->
     <div class="col-lg-12">
         <img src="/img\wien4.jpg" alt="wien" width="100%" height="313px">
     </div>
     
 
     <div class="col-lg-offset-2 col-lg-8">
-    <h2 style="color: #831213; font-family: Chivo !important">Contact Us Here:</h2>
-    <br>
-    <h4><a href="https://www.facebook.com/CodeFactoryVienna/">https://www.facebook.com/CodeFactoryVienna/</a></h4>
-    <h4>office@codefactory.wien</h4>
-    <h4 style="color: #831213;">+43 699 12255185</h4>
-    <br>
-    <img src="/img\logo.png" alt="LOGO" width="330px" height="120px">
+        <h2 style="color: #831213; font-family: Chivo !important">Contact Us Here:</h2>
+        <br>
+        <h4><a href="https://www.facebook.com/CodeFactoryVienna/">https://www.facebook.com/CodeFactoryVienna/</a></h4>
+        <h4>office@codefactory.wien</h4>
+        <h4 style="color: #831213;">+43 699 12255185</h4>
+        <br>
+        <img src="/img\logo.png" alt="LOGO" width="330px" height="120px">
     </div>
     
 </div>
